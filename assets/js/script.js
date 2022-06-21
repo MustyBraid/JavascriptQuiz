9718257d6deefd5b11713b7d49fa482a5209a233
Musty Braid <chartiercasey@gmail.com>
2022-06-21 01:48:03 -0400

    Last commit for the night. CSS is greatly improved, questions are all written, all that needs to be done still is finalizing high score list functionality. Very happy with this

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,27 +9,35 @@
 //that that would trivialize the journey to get a high score, so I'm randomizing the placement of the answer options and making a headache for myself
 
 //Not sure if it's best practice to write these in one line or many lines. I'm going with one line to prefer code legibility over question legibility
-let question = ['Javascript is a ________ language','How can a variable be declared as a constant?','']
-let rightA = ['Object-Oriented','const',]
-let wrongB = ['Object-Based','let',]
-let wrongC = ['Procedural','var',]
-let wrongD = ['Machine','constant',]
+let question = ['1. Javascript is a(n) ________ language','2. In Javascript, how can a variable be declared as a constant?','3. Which of the following is NOT a Javascript Framework?', '4. Upon encountering empty statements, what does the Javascript Interpreter do?', '5. What Javascript keyword is used to check whether a given property is valid or not?',"6. What does the Javascript 'debugger' statement do?","7. What is the output of print(NaN === NaN)?","8. Which function is used to serialize an object into a JSON string in Javascript?"] 
+let rightA = ['Object-Oriented','const','Cassandra','Ignores the statements','in','It acts as a breakpoint in a program','false','stringify()']
+let wrongB = ['Object-Based','let','Node','None of these','exists','It will debug all the errors in the program at runtime','true','JSONify()']
+let wrongC = ['Procedural','var','React','Gives a warning','is in','It will debug errors in the current statement if any exist','undefined','parse']
+let wrongD = ['Machine','constant','Vue','Throws an error','lies','All of these','Error','convert']
 let userScore = ''
 let buttonOrder = [0,1,2,3]
 var timeLeft = 120
-var JQueryButtons = $('#buttonsList li');
+var questionNumber = 0
 
 
 function wrongAnswer() {
-    alert('Wrong, you lose 5 seconds!')
+    $("#winLose").html('Wrong! You lose 5 seconds.');
+    $("#winLose").css('color','#ff0000');
     timeLeft -= 5;
 }
 
+//This somewhat inelligantly populates every button/question field with the needed information about the current question
  function randomizeAnswerPlacement(array) {
     $("#A").css('order', String(array[0]));
     $("#B").css('order', String(array[1]));
     $("#C").css('order', String(array[2]));
     $("#D").css('order', String(array[3]));
+    $("#A").html(rightA[questionNumber]);
+    $("#B").html(wrongB[questionNumber]);
+    $("#C").html(wrongC[questionNumber]);
+    $("#D").html(wrongD[questionNumber]);
+    $("#Question").html(question[questionNumber]);
+    
  }
 
 //This is a lovely function that randomizes the order of the elements of any array we give it
@@ -55,9 +63,16 @@ function shuffle(inputArray) {
 function correctAnswer() {
     //randomize button placements using shuffle
     shuffle(buttonOrder);
-    randomizeAnswerPlacement(buttonOrder);
     //advance to next step in central array of questions/answers
-    alert('You got it right! Good job');
+    questionNumber ++;
+    randomizeAnswerPlacement(buttonOrder);
+    if (questionNumber == question.length) {
+        endGame();
+        console.log('The game ended')
+        return;
+    }
+    $("#winLose").html('You got it right! Good job');
+    $("#winLose").css('color','green');
 }
 
 //Assign event listeners to the 'wrong' buttons and make them call wrongButton
